b9e883abc798a47a1edae14e223ecd2411c1a6da
kemptonburton <kemptonburton0@gmail.com>
2026-02-19 15:27:38 -0600

    working in dartwic

---
+++ dartwic_operator_console/DARTWIC/python_scripts/alarms/checks.py
@@ -0,0 +1,12 @@
+import dartwic as d
+
+#loop params
+|servervariables/loops.alarm_checks.target_loop_frequency.value| = 10.0
+#default not running - set to 1 outside of script to run autofill
+|servervariables/loops.alarm_checks.running.value| = 1 
+
+@d.on_loop("alarm_checks")
+def loop():
+
+    # put in sensor checks here
+    x = 1
---
+++ dartwic_operator_console/DARTWIC/python_scripts/control.py
@@ -0,0 +1,19 @@
+import dartwic as d
+
+# 0 = closed; 1 = open
+|controls/open_hand.last_known_state.value| = 0
+
+@d.on_function("toggle_open_hand")
+def toggle_open_hand(args):
+    #if last known was closed
+    if |controls/open_hand.last_known_state.value| == 0:
+        |controls/open_hand.last_known_state.value| = 1
+        # open hand
+        |hand_controller/relay_1_desired_state.value| = 1
+        
+
+    #if last known was open
+    else:
+        |controls/open_hand.last_known_state.value| = 0
+        # close hand
+        |hand_controller/relay_1_desired_state.value| = 0
