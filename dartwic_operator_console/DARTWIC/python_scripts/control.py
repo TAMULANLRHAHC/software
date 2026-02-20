a0106042e27e8fb31b9b6c09e99dfb4f538b8d68
kemptonburton <kemptonburton0@gmail.com>
2026-02-20 16:15:41 -0600

    added mappings and schematics

--- dartwic_operator_console/DARTWIC/python_scripts/control.py
+++ dartwic_operator_console/DARTWIC/python_scripts/control.py
@@ -1,5 +1,44 @@
 import dartwic as d
 
+
+### CONTROL MAPPINGS & DEFAULTS ###
+
+## DEFAULTS
+|servervariables/loops.dacs_loop.target_loop_frequency.value| = 10
+
+## FINGERS ##
+#pointer finger
+|hand_controller/relay_1_desired_state.mapped_channel| = "control/pointer_finger.extend.desired_state"
+|hand_controller/relay_1_actual_state.mapped_channel| = "control/pointer_finger.extend.actual_state"
+
+|hand_controller/relay_2_desired_state.mapped_channel| = "control/pointer_finger.retract.desired_state"
+|hand_controller/relay_2_actual_state.mapped_channel| = "control/pointer_finger.retract.actual_state"
+
+#other fingers
+|hand_controller/relay_3_desired_state.mapped_channel| = "control/hand_fingers.extend.desired_state"
+|hand_controller/relay_3_actual_state.mapped_channel| = "control/hand_fingers.extend.actual_state"
+
+|hand_controller/relay_4_desired_state.mapped_channel| = "control/hand_fingers.retract.desired_state"
+|hand_controller/relay_4_actual_state.mapped_channel| = "control/hand_fingers.retract.actual_state"
+
+## THUMB ##
+#rotational actuator
+|hand_controller/relay_5_desired_state.mapped_channel| = "control/thumb.extend.desired_state"
+|hand_controller/relay_5_actual_state.mapped_channel| = "control/thumb.extend.actual_state"
+
+|hand_controller/relay_6_desired_state.mapped_channel| = "control/thumb.retract.desired_state"
+|hand_controller/relay_6_actual_state.mapped_channel| = "control/thumb.retract.actual_state"
+
+#cylinder (opposition = perpendicular to palm, reposition = parallel to palm)
+|hand_controller/relay_7_desired_state.mapped_channel| = "control/thumb.opposition.desired_state"
+|hand_controller/relay_7_actual_state.mapped_channel| = "control/thumb.opposition.actual_state"
+
+|hand_controller/relay_8_desired_state.mapped_channel| = "control/thumb.reposition.desired_state"
+|hand_controller/relay_8_actual_state.mapped_channel| = "control/thumb.reposition.actual_state"
+
+
+### CUSTOM CONTROL FUNCTIONS ###
+
 # 0 = closed; 1 = open
 |controls/open_hand.last_known_state.value| = 0
 
---
+++ dartwic_operator_console/DARTWIC/python_scripts/sensors.py
@@ -0,0 +1,26 @@
+import dartwic as d
+
+### SENSOR MAPPINGS & DEFAULTS ###
+
+#hpu
+|hand_controller/AIN0_raw.mapped_channel| = "sensors/HPU.pressure"
+|hand_controller/AIN0_raw.scale| = 1
+|hand_controller/AIN0_raw.offset| = 0
+
+#finger cylinders
+|hand_controller/AIN1_raw.mapped_channel| = "sensors/pointer_finger.extension_cylinder.pressure"
+|hand_controller/AIN1_raw.scale| = 1
+|hand_controller/AIN1_raw.offset| = 0
+
+|hand_controller/AIN2_raw.mapped_channel| = "sensors/hand_fingers.extension_cylinder.pressure"
+|hand_controller/AIN2_raw.scale| = 1
+|hand_controller/AIN2_raw.offset| = 0
+
+#thumb cylinders & actuators
+|hand_controller/AIN3_raw.mapped_channel| = "sensors/thumb.extend_rotational_actuator.pressure"
+|hand_controller/AIN3_raw.scale| = 1
+|hand_controller/AIN3_raw.offset| = 0
+
+|hand_controller/AIN4_raw.mapped_channel| = "sensors/thumb.opposition_cylinder.pressure"
+|hand_controller/AIN4_raw.scale| = 1
+|hand_controller/AIN4_raw.offset| = 0
